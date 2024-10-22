484d4551eeac76cbda6fbfdfabae606e3accccf6
Tayami <tayamirai6@gmail.com>
2024-10-22 14:26:08 +0100

    Create the functioanlity to show tasks

--- Second View/toDoList.js
+++ Second View/toDoList.js
@@ -16,19 +16,20 @@ if(userName){
 
 }
 
-//Button generating a task
+//Create a task
 
 const taskBlock = document.getElementById('taskBlock')
 
+const taskBlockTitle = document.createElement('input')
+
+const taskBlockDescription = document.createElement('textarea')
 const createBtn = document.getElementById('createBtn')
 
+const submitTaskBtn = document.createElement('button')
+submitTaskBtn.innerHTML = "ADD"
 
 
 createBtn.addEventListener('click', ()=>{
-
-    const taskBlockTitle = document.createElement('input')
-
-    const taskBlockDescription = document.createElement('textarea')
 
     taskBlockTitle.placeholder = "Write task title"
     taskBlockDescription.placeholder = "Describe the task"
@@ -45,23 +46,46 @@ createBtn.addEventListener('click', ()=>{
     taskBlock.appendChild(taskBlockDescription)
 
     
+    submitTaskBtn.classList.add('submitTaskBtn')
 
-    taskBlock.appendChild(createAddBtn())
+    taskBlock.appendChild(submitTaskBtn)
+
 
 })
 
 
-function createAddBtn(){
-    const submitTaskBtn = document.createElement('button')
-    submitTaskBtn.innerHTML = "ADD"
-    submitTaskBtn.classList.add('submitTaskBtn')
+//Adding items to list
 
-    submitTaskBtn.addEventListener('click', ()=>{
-        alert("sods")
-    })
-
-    return submitTaskBtn
-}
+const listOfTasks = document.getElementById('listOfTasks')
 
 
+submitTaskBtn.addEventListener('click', ()=>{
+
+    const taskRow = document.createElement('div')
+    taskRow.classList.add('taskRow')
+
+    const task = document.createElement('div');
     
+    const taskTitle = document.createElement('li');
+    const taskDescription = document.createElement('li')
+
+    taskTitle.innerHTML = `${taskBlockTitle.value}`
+    taskTitle.style.fontWeight = 'bold'
+    taskDescription.innerHTML = `${taskBlockDescription.value}`
+
+    task.appendChild(taskTitle)
+    task.appendChild(taskDescription)
+
+    const doneButton = document.createElement('button')
+    doneButton.innerHTML = "DONE"
+
+    taskRow.appendChild(task)
+    taskRow.appendChild(doneButton)
+    
+    listOfTasks.appendChild(taskRow)
+    
+
+    taskBlockTitle.value = ""
+    taskBlockDescription.value = ""
+})
+
